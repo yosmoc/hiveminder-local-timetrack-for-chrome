324d24b08d2c611d5488dce694267e908caaa4ee
samurai20000 <samurai20000___@___gmail.com>
2010-08-13 11:49:02 +0900

    zip作成するように変更した。

--- build.rb
+++ build.rb
@@ -8,7 +8,14 @@ CrxMake.make(
   :pkey   => "./hiveminder-local-timetrack-for-chrome.pem",
   :crx_output => "./hiveminder-local-timetrack-for-chrome.crx",
   :verbose => true,
-  :ignorefile => /\.swp/,
   :ignoredir => /\.(?:svn|git|cvs)/
 )
 
+# create zip for Google Extension Gallery
+CrxMake.zip(
+  :ex_dir => "./src",
+  :pkey   => "./hiveminder-local-timetrack-for-chrome.pem",
+  :zip_output => "./hiveminder-local-timetrack-for-chrome.zip",
+  :verbose => true,
+  :ignoredir => /\.(?:svn|git|cvs)/
+)
